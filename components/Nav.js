33bfaa87fcdc691de1eb04b4c33b3f212c561cf9
mingyeongmo <mingyeongmp@naver.com>
2021-12-15 20:41:44 +0900

    module.css 적용

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,8 @@
 import Link from 'next/link'
-
+import navStyles from '../styles/Nav.module.css'
 const Nav = () => {
     return (
-        <nav>
+        <nav className={navStyles.nav}>
             <ul>
                 <li>
                     <Link href="/">
