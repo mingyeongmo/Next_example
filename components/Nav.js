9b9423eb85cf5c28792e55dea323c12ce798f1b3
mingyeongmo <mingyeongmp@naver.com>
2021-12-15 18:27:22 +0900

    Link와 props넘기기

---
+++ components/Nav.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/photos">
+                        <a>Photos</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+        
+    );
+};
+
+export default Nav;
